cb5df22e73fa4be0ad3ff794bacebc6514a3f4f4
Damon German <damon.german@hyland.com>
2021-07-05 15:06:27 -0500

    made some changes

--- WeatherGreeting/WeatherGreeting/Constants.cs
+++ WeatherGreeting/WeatherGreeting/Constants.cs
@@ -2,18 +2,22 @@
 {
     public static class Constants
     {
+        // locations
         public const string OverlandParkKansas = "Overland Park Kansas";
         public const string KansasCityMissouri = "Kansas City Missouri";
 
+        // Time of day greetings
         public const string TimeOfDayGreetingMorning = "Good morning.";
         public const string TimeOfDayGreetingAfterNoon = "Good after noon.";
         public const string TimeOfDayGreetingEvening = "Good evening.";
 
-        public const string TemperatureSuggestionHot = "It's hot out there, drink plenty of water.";
-        public const string TemperatureSuggestionCool = "You will want to wear a light jacket.";
-        public const string TemperatureSuggestionWarm = "Have fun.";
+        // Temperature suggestions
         public const string TemperatureSuggestionCold = "You will want to wear a coat.";
+        public const string TemperatureSuggestionCool = "You will want to wear a light jacket.";
+        public const string TemperatureSuggestionHot = "It's hot out there, drink plenty of water.";
+        public const string TemperatureSuggestionWarm = "Have fun.";
 
+        // UVIndex Suggestions
         public const string SunscreenSuggestionMedium = "Consider wearing sunscreen.";
         public const string SunscreenSuggestionHigh = "You definitely should wear sunscreen!";
     }
