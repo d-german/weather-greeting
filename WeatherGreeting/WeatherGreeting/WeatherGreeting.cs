c2c3966d9cc0ab41ca35aa674df5bdfa4fe27f65
Damon German <damon.german@hyland.com>
2021-07-05 14:48:14 -0500

    made some changes

--- WeatherGreeting/WeatherGreeting/WeatherGreeting.cs
+++ WeatherGreeting/WeatherGreeting/WeatherGreeting.cs
@@ -18,23 +18,24 @@ namespace WeatherGreeting
             var weatherData = _weatherService.FetchWeatherData(mapPoint, DateTime.Now);
 
             var timeOfDayGreeting = string.Empty;
-            var currentTemperatureGreeting = $"The current temperature is {weatherData.Temperature}";
-            var currentTemperatureSuggestion = string.Empty;
+            var temperatureStatement = $"The current temperature is {weatherData.Temperature} degrees fahrenheit.";
+            var temperatureSuggestion = string.Empty;
+            var sunscreenSuggestion = string.Empty;
 
             if (weatherData.DateTime.HasValue)
             {
                 var hour = weatherData.DateTime.Value.Hour;
                 if (hour < 12)
                 {
-                    timeOfDayGreeting = "Good morning";
+                    timeOfDayGreeting = "Good morning.";
                 }
                 else if (hour < 16)
                 {
-                    timeOfDayGreeting = "Good after noon";
+                    timeOfDayGreeting = "Good after noon.";
                 }
                 else
                 {
-                    timeOfDayGreeting = "Good evening";
+                    timeOfDayGreeting = "Good evening.";
                 }
             }
 
@@ -44,24 +45,42 @@ namespace WeatherGreeting
                 {
                     // hot
                     case > 80:
-                        currentTemperatureSuggestion = "It's hot out there, drink plenty of water";
+                        temperatureSuggestion = "It's hot out there, drink plenty of water.";
                         break;
                     // Warm
                     case > 70:
-                        currentTemperatureSuggestion = "Have fun";
+                        temperatureSuggestion = "Have fun.";
                         break;
                     // Cool
                     case > 60:
-                        currentTemperatureSuggestion = "You will want to wear a light jacket";
+                        temperatureSuggestion = "You will want to wear a light jacket.";
                         break;
                     // Cold
                     default:
-                        currentTemperatureSuggestion = "You will want to wear a coat";
+                        temperatureSuggestion = "You will want to wear a coat.";
                         break;
                 }
             }
 
-            _greetingService.TransmitGreeting($"{timeOfDayGreeting}. {currentTemperatureGreeting}. {currentTemperatureSuggestion}.");
+            if (weatherData.Temperature.HasValue && weatherData.UvIndex.HasValue)
+            {
+                var temperature = weatherData.Temperature.Value;
+                var uvIndex = weatherData.UvIndex.Value;
+
+                if (temperature > 70)
+                {
+                    if (uvIndex > 3 && uvIndex < 5)
+                    {
+                        sunscreenSuggestion = "Consider wearing sunscreen.";
+                    }
+                    else if (uvIndex > 5)
+                    {
+                        sunscreenSuggestion = "You definitely should wear sunscreen!";
+                    }
+                }
+            }
+
+            _greetingService.TransmitGreeting($"{timeOfDayGreeting} {temperatureStatement} {temperatureSuggestion} {sunscreenSuggestion}");
         }
     }
 }
