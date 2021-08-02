a674ec0bf3ccfa24a02a16bdfcdc2b5499b37750
Damon German <damon.german@hyland.com>
2021-08-02 11:36:06 -0500

    made some changes

--- WeatherGreeting/WeatherGreetingTests/WeatherGreetingTests.cs
+++ WeatherGreeting/WeatherGreetingTests/WeatherGreetingTests.cs
@@ -35,29 +35,78 @@ namespace WeatherGreetingTests
         }
     }
 
+    public record WeatherGreetingTestCase
+    {
+        public double Temperature { get; init; }
+        public DateTime DateTime { get; init; }
+        public int UvIndex { get; init; }
+        public string ExpectedResult { get; init; }
+    }
+
     [TestFixture]
     public class WeatherGreetingTests
     {
-        // MorningHotLowUvIndexTest
-        // MorningHotMediumUvIndexTest
-        // MorningHotHighUvIndexTest
-        // MorningWarmLowUvIndexTest
-        // MorningWarmMediumUvIndexTest
-        // MorningWarmHighUvIndexTest
+        private static WeatherGreetingTestCase[] _weatherGreetingTestCases =
+        {
+            // MorningHotLowUvIndexTest
+            new()
+            {
+                Temperature = 100,
+                DateTime = DateTime.Parse("03/01/2009 09:00:00"), // 9AM needs to be morning
+                UvIndex = 1,
+                ExpectedResult = "Good morning. The current temperature is 100 degrees fahrenheit. It's hot out there, drink plenty of water. "
+            }
+            // MorningHotMediumUvIndexTest
+            // MorningHotHighUvIndexTest
+            // MorningWarmLowUvIndexTest
+            // MorningWarmMediumUvIndexTest
+            // MorningWarmHighUvIndexTest
 
-        // AfterNoonHotLowUvIndexTest
-        // AfterNoonHotMediumUvIndexTest
-        // AfterNoonHotHighUvIndexTest
-        // AfterNoonWarmLowUvIndexTest
-        // AfterNoonWarmMediumUvIndexTest
-        // AfterNoonWarmHighUvIndexTest
+            // AfterNoonHotLowUvIndexTest
+            // AfterNoonHotMediumUvIndexTest
+            // AfterNoonHotHighUvIndexTest
+            // AfterNoonWarmLowUvIndexTest
+            // AfterNoonWarmMediumUvIndexTest
+            // AfterNoonWarmHighUvIndexTest
 
-        // EveningHotLowUvIndexTest
-        // EveningHotMediumUvIndexTest
-        // EveningHotHighUvIndexTest
-        // EveningWarmLowUvIndexTest
-        // EveningWarmMediumUvIndexTest
-        // EveningWarmHighUvIndexTest
+            // EveningHotLowUvIndexTest
+            // EveningHotMediumUvIndexTest
+            // EveningHotHighUvIndexTest
+            // EveningWarmLowUvIndexTest
+            // EveningWarmMediumUvIndexTest
+            // EveningWarmHighUvIndexTest
+        };
+
+        [TestCaseSource(nameof(_weatherGreetingTestCases))]
+        public void WeatherGreetingTest(WeatherGreetingTestCase testCase)
+        {
+            var mockWeatherService = new MockWeatherService
+            {
+                MockWeatherData = new WeatherData
+                {
+                    Temperature = testCase.Temperature,
+                    DateTime = testCase.DateTime,
+                    UvIndex = testCase.UvIndex
+                }
+            };
+
+            var mockLocationService = new MockLocationService
+            {
+                MockMapPoint = new MapPoint() // don't care can be any value
+            };
+
+            var mockGreetingService = new MockGreetingService();
+
+            var sut = new WeatherGreeting.WeatherGreeting(mockGreetingService, mockWeatherService, mockLocationService);
+
+            var actualGreeting = sut.TransmitGreeting(string.Empty, DateTime.Now);
+            var expectedGreeting = testCase.ExpectedResult;
+            var actualGreetingServiceGreeting = mockGreetingService.MockGreetingToVerify;
+
+            Assert.That(actualGreeting, Is.EqualTo(expectedGreeting));
+            Assert.That(actualGreetingServiceGreeting, Is.EqualTo(expectedGreeting));
+        }
+
 
         [Test]
         public void MorningHotHighUvIndexTest()
