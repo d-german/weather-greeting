a78f5e518ec90d734b8292bdee28d71702bd5b3e
Damon German <damon.german@hyland.com>
2021-08-20 15:21:24 -0500

    made some changes

--- WeatherGreeting/WeatherGreetingTests/WeatherServiceMetricsDecoratorTests.cs
+++ WeatherGreeting/WeatherGreetingTests/WeatherServiceMetricsDecoratorTests.cs
@@ -14,8 +14,11 @@ namespace WeatherGreetingTests
         {
             //TODO: uncomment and use the next line
             // var sut = new WeatherServiceMetricsDecorator(new MockWeatherService());
+
             //TODO: write tests for 0, 1, and 2 calls to FetchWeatherData()
-            // Use the WeatherServiceRequestCount property  to make the assertions.
+            // Use the WeatherServiceRequestCount property to make the assertions.
+            // If I have made no calls to FetchWeatherData() then WeatherServiceRequestCount should be 0.
+            // If I made one, the WeatherServiceRequestCount should be 1, etc.
         }
     }
 }
