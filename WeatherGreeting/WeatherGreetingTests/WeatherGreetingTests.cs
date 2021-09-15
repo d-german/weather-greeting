10eef9d952c4aa78a958b12119fe6d3b50c68888
Damon German <damon.german@hyland.com>
2021-09-15 08:22:56 -0500

    made some changes

--- WeatherGreeting/WeatherGreetingTests/WeatherGreetingTests.cs
+++ WeatherGreeting/WeatherGreetingTests/WeatherGreetingTests.cs
@@ -35,42 +35,229 @@ namespace WeatherGreetingTests
         }
     }
 
+    public record WeatherGreetingTestCase
+    {
+        public double Temperature { get; init; }
+        public DateTime DateTime { get; init; }
+        public int UvIndex { get; init; }
+        public string ExpectedResult { get; init; }
+    }
+
     [TestFixture]
     public class WeatherGreetingTests
     {
-        // MorningHotLowUvIndexTest
-        // MorningHotMediumUvIndexTest
-        // MorningHotHighUvIndexTest
-        // MorningWarmLowUvIndexTest
-        // MorningWarmMediumUvIndexTest
-        // MorningWarmHighUvIndexTest
+        private static WeatherGreetingTestCase[] _weatherGreetingTestCases =
+        {
+            // MorningHotLowUvIndexTest
+            new()
+            {
+                Temperature = 100,
+                DateTime = DateTime.Parse("03/01/2009 09:00:00"), // 9AM needs to be morning
+                UvIndex = 1,
+                ExpectedResult = "Good morning. The current temperature is 100 degrees fahrenheit. It's hot out there, drink plenty of water."
+            },
+            // MorningHotMediumUvIndexTest
+            new()
+            {
+                Temperature = 100,
+                DateTime = DateTime.Parse("03/01/2009 09:00:00"), // 9AM needs to be morning
+                UvIndex = 4,
+                ExpectedResult = "Good morning. The current temperature is 100 degrees fahrenheit. It's hot out there, drink plenty of water. Consider wearing sunscreen."
+            },
+            // MorningHotHighUvIndexTest
+            new()
+            {
+                Temperature = 100,
+                DateTime = DateTime.Parse("03/01/2009 09:00:00"),
+                UvIndex = 6,
+                ExpectedResult = "Good morning. The current temperature is 100 degrees fahrenheit. It's hot out there, drink plenty of water. You definitely should wear sunscreen!"
+            },
 
-        // AfterNoonHotLowUvIndexTest
-        // AfterNoonHotMediumUvIndexTest
-        // AfterNoonHotHighUvIndexTest
-        // AfterNoonWarmLowUvIndexTest
-        // AfterNoonWarmMediumUvIndexTest
-        // AfterNoonWarmHighUvIndexTest
 
-        // EveningHotLowUvIndexTest
-        // EveningHotMediumUvIndexTest
-        // EveningHotHighUvIndexTest
-        // EveningWarmLowUvIndexTest
-        // EveningWarmMediumUvIndexTest
-        // EveningWarmHighUvIndexTest
+            // MorningWarmLowUvIndexTest
+            new()
+            {
+                Temperature = 75,
+                DateTime = DateTime.Parse("03/01/2009 09:00:00"),
+                UvIndex = 1,
+                ExpectedResult = "Good morning. The current temperature is 75 degrees fahrenheit. Have fun."
+            },
+            // MorningWarmMediumUvIndexTest
+            new()
+            {
+                Temperature = 75,
+                DateTime = DateTime.Parse("03/01/2009 09:00:00"),
+                UvIndex = 4,
+                ExpectedResult = "Good morning. The current temperature is 75 degrees fahrenheit. Have fun. Consider wearing sunscreen."
+            },
+            // MorningWarmHighUvIndexTest
+            new()
+            {
+                Temperature = 75,
+                DateTime = DateTime.Parse("03/01/2009 09:00:00"),
+                UvIndex = 6,
+                ExpectedResult = "Good morning. The current temperature is 75 degrees fahrenheit. Have fun. You definitely should wear sunscreen!"
+            },
+            // MorningCoolAnyUvIndexTest
+            new()
+            {
+                Temperature = 65,
+                DateTime = DateTime.Parse("03/01/2009 09:00:00"),
+                UvIndex = 6,
+                ExpectedResult = "Good morning. The current temperature is 65 degrees fahrenheit. You will want to wear a light jacket."
+            },
+            // MorningColdAnyUvIndexTest
+            new()
+            {
+                Temperature = 41,
+                DateTime = DateTime.Parse("03/01/2009 09:00:00"),
+                UvIndex = 1,
+                ExpectedResult = "Good morning. The current temperature is 41 degrees fahrenheit. You will want to wear a coat."
+            },
 
-        [Test]
-        public void MorningHotHighUvIndexTest()
+            // AfterNoonHotLowUvIndexTest
+            new()
+            {
+                Temperature = 99,
+                DateTime = DateTime.Parse("03/01/2009 13:00:00"),
+                UvIndex = 1,
+                ExpectedResult = "Good after noon. The current temperature is 99 degrees fahrenheit. It's hot out there, drink plenty of water."
+            },
+            // AfterNoonHotMediumUvIndexTest
+            new()
+            {
+                Temperature = 99,
+                DateTime = DateTime.Parse("03/01/2009 13:00:00"),
+                UvIndex = 4,
+                ExpectedResult = "Good after noon. The current temperature is 99 degrees fahrenheit. It's hot out there, drink plenty of water. Consider wearing sunscreen."
+            },
+            // AfterNoonHotHighUvIndexTest
+            new()
+            {
+                Temperature = 99,
+                DateTime = DateTime.Parse("03/01/2009 13:00:00"),
+                UvIndex = 6,
+                ExpectedResult = "Good after noon. The current temperature is 99 degrees fahrenheit. It's hot out there, drink plenty of water. You definitely should wear sunscreen!"
+            },
+            // AfterNoonWarmLowUvIndexTest
+            new()
+            {
+                Temperature = 73,
+                DateTime = DateTime.Parse("03/01/2009 13:00:00"),
+                UvIndex = 2,
+                ExpectedResult = "Good after noon. The current temperature is 73 degrees fahrenheit. Have fun."
+            },
+            // AfterNoonWarmMediumUvIndexTest
+            new()
+            {
+                Temperature = 73,
+                DateTime = DateTime.Parse("03/01/2009 13:00:00"),
+                UvIndex = 4,
+                ExpectedResult = "Good after noon. The current temperature is 73 degrees fahrenheit. Have fun. Consider wearing sunscreen."
+            },
+            // AfterNoonWarmHighUvIndexTest
+            new()
+            {
+                Temperature = 73,
+                DateTime = DateTime.Parse("03/01/2009 13:00:00"),
+                UvIndex = 6,
+                ExpectedResult = "Good after noon. The current temperature is 73 degrees fahrenheit. Have fun. You definitely should wear sunscreen!"
+            },
+            // AfterNoonCoolAnyUvIndexTest
+            new()
+            {
+                Temperature = 68,
+                DateTime = DateTime.Parse("03/01/2009 13:00:00"),
+                UvIndex = 6,
+                ExpectedResult = "Good after noon. The current temperature is 68 degrees fahrenheit. You will want to wear a light jacket."
+            },
+            // AfterNoonColdAnyUvIndexTest
+            new()
+            {
+                Temperature = 21,
+                DateTime = DateTime.Parse("03/01/2009 13:00:00"),
+                UvIndex = 4,
+                ExpectedResult = "Good after noon. The current temperature is 21 degrees fahrenheit. You will want to wear a coat."
+            },
+            // EveningHotLowUvIndexTest
+            new()
+            {
+                Temperature = 97,
+                DateTime = DateTime.Parse("03/01/2009 17:00:00"),
+                UvIndex = 3,
+                ExpectedResult = "Good evening. The current temperature is 97 degrees fahrenheit. It's hot out there, drink plenty of water."
+            },
+            // EveningHotMediumUvIndexTest
+            new()
+            {
+                Temperature = 97,
+                DateTime = DateTime.Parse("03/01/2009 17:00:00"),
+                UvIndex = 4,
+                ExpectedResult = "Good evening. The current temperature is 97 degrees fahrenheit. It's hot out there, drink plenty of water. Consider wearing sunscreen."
+            },
+            // EveningHotHighUvIndexTest
+            new()
+            {
+                Temperature = 97,
+                DateTime = DateTime.Parse("03/01/2009 17:00:00"),
+                UvIndex = 7,
+                ExpectedResult = "Good evening. The current temperature is 97 degrees fahrenheit. It's hot out there, drink plenty of water. You definitely should wear sunscreen!"
+            },
+            // EveningWarmLowUvIndexTest
+            new()
+            {
+                Temperature = 79,
+                DateTime = DateTime.Parse("03/01/2009 17:00:00"),
+                UvIndex = 1,
+                ExpectedResult = "Good evening. The current temperature is 79 degrees fahrenheit. Have fun."
+            },
+            // EveningWarmMediumUvIndexTest
+            new()
+            {
+                Temperature = 79,
+                DateTime = DateTime.Parse("03/01/2009 17:00:00"),
+                UvIndex = 4,
+                ExpectedResult = "Good evening. The current temperature is 79 degrees fahrenheit. Have fun. Consider wearing sunscreen."
+            },
+            // EveningWarmHighUvIndexTest
+            new()
+            {
+                Temperature = 79,
+                DateTime = DateTime.Parse("03/01/2009 17:00:00"),
+                UvIndex = 7,
+                ExpectedResult = "Good evening. The current temperature is 79 degrees fahrenheit. Have fun. You definitely should wear sunscreen!"
+            },
+            // EveningCoolAnyUvIndexTest
+            new()
+            {
+                Temperature = 62,
+                DateTime = DateTime.Parse("03/01/2009 17:00:00"),
+                UvIndex = 7,
+                ExpectedResult = "Good evening. The current temperature is 62 degrees fahrenheit. You will want to wear a light jacket."
+            },
+            // EveningColdAnyUvIndexTest
+            new()
+            {
+                Temperature = -5,
+                DateTime = DateTime.Parse("03/01/2009 17:00:00"),
+                UvIndex = 4,
+                ExpectedResult = "Good evening. The current temperature is -5 degrees fahrenheit. You will want to wear a coat."
+            },
+        };
+
+        [TestCaseSource(nameof(_weatherGreetingTestCases))]
+        public void WeatherGreetingTest(WeatherGreetingTestCase testCase)
         {
             var mockWeatherService = new MockWeatherService
             {
                 MockWeatherData = new WeatherData
                 {
-                    Temperature = 100,
-                    DateTime = DateTime.Parse("03/01/2009 09:00:00"), // 9AM needs to be morning
-                    UvIndex = 10
+                    Temperature = testCase.Temperature,
+                    DateTime = testCase.DateTime,
+                    UvIndex = testCase.UvIndex
                 }
             };
+
             var mockLocationService = new MockLocationService
             {
                 MockMapPoint = new MapPoint() // don't care can be any value
@@ -81,9 +268,7 @@ namespace WeatherGreetingTests
             var sut = new WeatherGreeting.WeatherGreeting(mockGreetingService, mockWeatherService, mockLocationService);
 
             var actualGreeting = sut.TransmitGreeting(string.Empty, DateTime.Now);
-            const string expectedGreeting = "Good morning. The current temperature is 100 degrees fahrenheit. " +
-                                            "It's hot out there, drink plenty of water. " +
-                                            "You definitely should wear sunscreen!";
+            var expectedGreeting = testCase.ExpectedResult;
             var actualGreetingServiceGreeting = mockGreetingService.MockGreetingToVerify;
 
             Assert.That(actualGreeting, Is.EqualTo(expectedGreeting));
