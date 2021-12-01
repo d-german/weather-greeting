cf1843c4ec609b4b8761d81ce139a883fa66764e
Damon German <damon.german@hyland.com>
2021-12-01 11:50:13 -0600

    made some changes

--- WeatherGreeting/WeatherGreeting/Program.cs
+++ WeatherGreeting/WeatherGreeting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 using WeatherGreeting.Services;
 using static WeatherGreeting.Constants;
 
@@ -8,21 +9,34 @@ namespace WeatherGreeting
     {
         private static void Main()
         {
+            var serviceProvider = ConfigureServiceProvider();
+
             Console.WriteLine("Choose Location");
             Console.WriteLine($"1 for {KansasCityMissouri}");
             Console.WriteLine($"2 for {OverlandParkKansas}");
 
             var location = Console.ReadLine() == "1" ? KansasCityMissouri : OverlandParkKansas;
-            var greeting = new WeatherGreeting(
-                new GreetingService(),
-                new WeatherServiceMetricsDecorator(new WeatherService()),
-                new LocationService());
+            var greeting = serviceProvider.GetRequiredService<WeatherGreeting>();
+
             Console.WriteLine("Press Enter to keep going or any other key to Exit");
 
             do
             {
                 greeting.TransmitGreeting(location);
             } while (Console.ReadKey().Key == ConsoleKey.Enter);
+        }
+
+        private static ServiceProvider ConfigureServiceProvider()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<WeatherGreeting>();
+            services.AddSingleton<IGreetingService, GreetingService>();
+            services.AddSingleton<ILocationService, LocationService>();
+            services.AddSingleton<IWeatherService>(
+                c => ActivatorUtilities.CreateInstance<WeatherServiceMetricsDecorator>(
+                    c, ActivatorUtilities.CreateInstance<WeatherService>(c)));
+
+            return services.BuildServiceProvider();
         }
     }
 }
