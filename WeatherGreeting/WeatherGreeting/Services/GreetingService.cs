1fb947f0d769d4ab89ec47667a91a6e63639a342
Damon German <damon.german@hyland.com>
2022-04-25 14:43:51 -0500

    made some changes

--- WeatherGreeting/WeatherGreeting/Services/GreetingService.cs
+++ WeatherGreeting/WeatherGreeting/Services/GreetingService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace WeatherGreeting.Services
 {
@@ -14,6 +15,35 @@ namespace WeatherGreeting.Services
         public void TransmitGreeting(string greeting)
         {
             Console.WriteLine($"#{_id++}:={greeting}");
+        }
+    }
+
+    public class GreetingServiceFileWriteDecorator : IGreetingService
+    {
+        private readonly IGreetingService _greetingServiceDecoratee;
+
+        public GreetingServiceFileWriteDecorator(IGreetingService greetingServiceDecoratee)
+        {
+            _greetingServiceDecoratee = greetingServiceDecoratee;
+        }
+
+        public void TransmitGreeting(string greeting)
+        {
+            try
+            {
+                using var file = new FileStream("greeting.txt", FileMode.Append);
+                using var writer = new StreamWriter(file);
+                writer.WriteLine(greeting);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                _greetingServiceDecoratee.TransmitGreeting(greeting);
+            }
         }
     }
 }
